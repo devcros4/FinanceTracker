aa45ab1f91d6502b44d23590e2ab4c7e209ad3be
Jean-Baptiste <jbdelcros4@hotmail.fr>
2022-10-23 16:05:31 -0400

    Initial Commit

---
+++ Financetracker/FinancetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancetrackerApp.swift
+//  Financetracker
+//
+//  Created by jean-baptiste delcros on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinancetrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
